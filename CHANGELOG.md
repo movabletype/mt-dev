c9926cee7de8754f557cd2eacc913783c5f05ae0
Taku Amano <taku@taaas.jp>
2020-10-21 11:26:46 +0900

    Support relative CGIPath/StaticWebPath.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+* Support relative CGIPath/StaticWebPath.
+
 ## [1.0.2] - 2020-10-15
 
 ### Fixed
--- mt/docker-entrypoint.sh
+++ mt/docker-entrypoint.sh
@@ -7,13 +7,43 @@ chmod 777 /var/www/cgi-bin/mt/theme
 if [ "$1" = "apache2-foreground" ]; then
     rm -f /var/log/apache2/access.log # disable access logging
 
-    httpd_conf_d=`test -e /etc/httpd/conf.d && echo /etc/httpd/conf.d || echo '/etc/apache2/conf-enabled'`
+    httpd_conf_d=`ls -d /etc/httpd/conf.d /etc/apache2/conf-enabled /etc/httpd/conf/extra 2>/dev/null | head -1`
     cat > $httpd_conf_d/mt.conf <<CONF
 Timeout 3600
 
 # mt-static
 Alias /mt-static/ /var/www/cgi-bin/mt/mt-static/
 CONF
+
+    mod_rewrite_so=`find /usr/lib/apache2/modules /usr/lib64/httpd/modules /usr/lib/httpd/modules -name 'mod_rewrite.so' 2>/dev/null | head -1`
+    if [ -n "$mod_rewrite_so" ]; then
+        cat > $httpd_conf_d/mt-rewrite.conf <<CONF
+LoadModule rewrite_module $mod_rewrite_so
+CONF
+    fi
+
+    mod_proxy_so=`find /usr/lib/apache2/modules /usr/lib64/httpd/modules /usr/lib/httpd/modules -name 'mod_proxy.so' 2>/dev/null | head -1`
+    if [ -n "$mod_proxy_so" ]; then
+        cat > $httpd_conf_d/mt-proxy.conf <<CONF
+LoadModule proxy_module $mod_proxy_so
+ProxyPassReverse / http://mt/
+CONF
+    fi
+
+    mod_proxy_http_so=`find /usr/lib/apache2/modules /usr/lib64/httpd/modules /usr/lib/httpd/modules -name 'mod_proxy_http.so' 2>/dev/null | head -1`
+    if [ -n "$mod_proxy_http_so" ]; then
+        cat > $httpd_conf_d/mt-proxy_http.conf <<CONF
+LoadModule proxy_http_module $mod_proxy_http_so
+ProxyPassReverse / http://mt/
+CONF
+    fi
+
+    if [ $httpd_conf_d = "/etc/httpd/conf/extra" ]; then
+        # archlinux
+        cat >> /etc/httpd/conf/httpd.conf <<CONF
+Include conf/extra/mt*.conf
+CONF
+    fi
 
     # invoke php-fpm
     if [ -e /usr/sbin/php-fpm ]; then
