619d70b0aac5b6dee09eebb3f019130877971fc0
Taku Amano <taku@taaas.jp>
2020-10-22 10:44:01 +0900

    Set NLS_LANG=Japanese_Japan.UTF8 for oracle.

--- mt/docker-entrypoint.sh
+++ mt/docker-entrypoint.sh
@@ -57,6 +57,19 @@ CONF
         exec /usr/sbin/httpd -D FOREGROUND
     fi
 else
+    httpd_conf_d=`ls -d /etc/httpd/conf.d /etc/apache2/conf-enabled /etc/httpd/conf/extra 2>/dev/null | head -1`
+    cat > $httpd_conf_d/mt.conf <<CONF
+Timeout 3600
+CONF
+
+    mod_env_so=`find /usr/lib/apache2/modules /usr/lib64/httpd/modules /usr/lib/httpd/modules -name 'mod_env.so' 2>/dev/null | head -1`
+    if [ -n "$mod_env_so" ]; then
+        cat > $httpd_conf_d/mt-env.conf <<CONF
+LoadModule env_module $mod_env_so
+PassEnv NLS_LANG
+CONF
+    fi
+
     if [ -e "/var/www/cgi-bin/mt/Makefile" ]; then
         make -C /var/www/cgi-bin/mt clean me
     fi
