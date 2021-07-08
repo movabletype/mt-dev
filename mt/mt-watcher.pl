56d6793d5fd30293c82374abe764342532e3ef4a
Taku Amano <taku@taaas.jp>
2021-07-09 05:30:52 +0900

    Use Filesys::Notify::Simple instead of -R option.

---
+++ mt/mt-watcher.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Filesys::Notify::Simple;
+
+# wait for `make me` to complete
+sleep(5);
+
+my @files   = ( qw(addons extlib lib plugins), glob('*.cgi') );
+my $watcher = Filesys::Notify::Simple->new( \@files );
+while (1) {
+    $watcher->wait(
+        sub {
+            my @events = @_;
+            print( join( "\n", map { $_->{path} } @events ) . "\n" );
+            system('docker kill -s HUP mt_mt_1');
+
+            # throttling
+            sleep(1);
+        }
+    );
+}
