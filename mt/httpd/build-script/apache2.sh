c40a7b8b26539b462510ade856f21da9c779a256
Keita Jamadam Sugama <sugama@jamadam.com>
2022-09-09 15:12:43 +0900

    set NLS_LANG for oracle

--- mt/httpd/build-script/apache2.sh
+++ mt/httpd/build-script/apache2.sh
@@ -57,3 +57,11 @@ if [ $httpd_conf_d = "/etc/httpd/conf/extra" ]; then
 Include conf/extra/mt*.conf
 CONF
 fi
+
+mod_env_so=`find $module_dirs -name 'mod_env.so' 2>/dev/null | head -1`
+if [ -n "$mod_env_so" ]; then
+    cat > $httpd_conf_d/mt-env.conf <<CONF
+LoadModule env_module $mod_env_so
+PassEnv NLS_LANG
+CONF
+fi
